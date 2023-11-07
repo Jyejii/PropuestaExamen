5f22295bcce00b92efbb5e68b2ef013bff96618a
Jiachen <146822199+Jyejii@users.noreply.github.com>
2023-11-07 11:39:17 +0100

    Propuesta de examen (07/11/2023)

---
+++ TriplesPitagoricos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a ;
+    int b;
+    int c;
+    for ( a= 1; a <= 17;a++) {
+        for (b=a; b<=17;b++){
+            c =sqrt(a*a+b*b);
+
+            if( c<= 17 ){
+                if( a*a+b*b==c* c && a+ b>c && a + c > b && b + c > a){
+                    std::cout<< a << "-" << b <<"-"<< c <<std::endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
